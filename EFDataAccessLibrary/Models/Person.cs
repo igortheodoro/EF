89c167de320074f2d52855349520129f8c3c047f
Igor Theodoro <igortheodoro12@gmail.com>
2020-08-17 15:57:43 -0300

    first commit

---
+++ EFDataAccessLibrary/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFDataAccessLibrary.Models
+{
+    public class Person
+    {   
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+        public IList<Address> Addresses { get; set; } = new List<Address>();
+        public IList<Email> EmailAddresses { get; set; } = new List<Email>();
+    }
+}
---
+++ EFDemoWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace EFDemoWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext context;
+
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext context)
+        {
+            _logger = logger;
+            this.context = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        private void LoadSampleData()
+        {
+            if (context.People.Count() == 0)
+            {
+                string file = System.IO.File.ReadAllText("generated.json");
+
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+
+                context.AddRange(people);
+                context.SaveChanges();
+            }
+        }
+    }
+}
